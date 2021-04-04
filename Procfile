7969a3f93d8d4263ca5779a3e730bc4f8bb8e094
Yury Shashurin <shashurin.edu@mail.ru>
2021-04-04 19:29:22 +0300

    deploy2

--- Procfile
+++ Procfile
@@ -1,2 +1,4 @@
 release: set FLASK_APP=manage.py
+release: alembic revision --autogenerate -m "initial migration"
+release: alembic upgrade head
 web: python manage.py 0.0.0.0:$PORT
--- config.py
+++ config.py
@@ -2,10 +2,12 @@ import os
 
 class Config:
 
-    DEBUG = False
+    DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:as89lokan07@localhost:5432/eventt'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASK_APP = "manage.py"
-    FLASK_DEBUG = 0
+    FLASK_DEBUG = 1
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    # SECRET_KEY = '%RZzk)j8e*:3ZxWnr<O=TZ7K:Z<1_0/sXYL~?{Okc!GpgX"Ava2NEFQLjo}I4Iy'
     DEBUG_TB_INTERCEPT_REDIRECTS = False

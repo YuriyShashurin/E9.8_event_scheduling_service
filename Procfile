a070e512f823544b6ad482397f495b538d404343
Yury Shashurin <shashurin.edu@mail.ru>
2021-04-04 19:57:59 +0300

    deploy5

--- Procfile
+++ Procfile
@@ -1,4 +1,5 @@
 release: set FLASK_APP=manage.py
 release: flask db migrate -m "Initial migration."
 release: flask db upgrade
-web: flask run 0.0.0.0:$PORT
+web: gunicorn app:app
+
--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:as89lokan07@localhost:5432/eventtt'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    FLASK_APP = "manage.py"
+    FLASK_APP = "app.py"
     FLASK_DEBUG = 0
     # SECRET_KEY = os.environ.get('SECRET_KEY')
     SECRET_KEY = '%RZzk)j8e*:3ZxWnr<O=TZ7K:Z<1_0/sXYL~?{Okc!GpgX"Ava2NEFQLjo}I4Iy'
